ed171daaed1789db9b399c30152ee9a7cef6f49e
Lucas Silva <lucas.cristiano96@gmail.com>
2025-01-27 22:31:19 -0300

    Implementado e manipulado lista de jogos

--- src/main/kotlin/br/com/alura/alugames/main/Main.kt
+++ src/main/kotlin/br/com/alura/alugames/main/Main.kt
@@ -1,42 +1,84 @@
 package br.com.alura.alugames.main
 
+import br.com.alura.alugames.model.Gamer
 import br.com.alura.alugames.model.Jogo
 import br.com.alura.alugames.service.ConsumoApi
 import java.util.*
 
 fun main() {
     val sc = Scanner(System.`in`)
-    println("Digite um código de jogo para buscar:")
-    val busca = sc.nextLine()
+    val gamer = Gamer.criarGamer(sc)
+    println("Cadastro Concluído com Sucesso! Dados do Gamer:")
+    println(gamer)
 
-    val buscaApi = ConsumoApi()
+    do {
+        println("Digite um código de jogo para buscar:")
+        val busca = sc.nextLine()
 
-    var meuJogo: Jogo? = null
+        val buscaApi = ConsumoApi()
 
-    val resultado = runCatching {
-        val meuInfoJogo = buscaApi.buscaJogo(busca)
-        meuJogo = Jogo(
-            meuInfoJogo.info.title,
-            meuInfoJogo.info.thumb)
-    }
+        var meuJogo: Jogo? = null
 
-    resultado.onFailure {
-        println("Jogo inexistente. Tente outro id.")
-    }
-
-    resultado.onSuccess {
-        println("Deseja inserir uma descrição personalizada? (S/N)")
-        val opcao = sc.nextLine()
-        if (opcao.equals("s", true)) {
-            println("Insira a descrição perzonalizada para o jogo:")
-            val descricaoPersonalizada = sc.nextLine()
-            meuJogo?.descricao = descricaoPersonalizada
-        } else {
-            meuJogo?.descricao = meuJogo?.titulo
+        val resultado = runCatching {
+            val meuInfoJogo = buscaApi.buscaJogo(busca)
+            meuJogo = Jogo(
+                meuInfoJogo.info.title,
+                meuInfoJogo.info.thumb)
         }
-        println(meuJogo)
+
+        resultado.onFailure {
+            println("Jogo inexistente. Tente outro id.")
+        }
+
         resultado.onSuccess {
-            println("Busca finalizada com sucesso!")
+            println("Deseja inserir uma descrição personalizada? (S/N)")
+            val opcao = sc.nextLine()
+            if (opcao.equals("s", true)) {
+                println("Insira a descrição perzonalizada para o jogo:")
+                val descricaoPersonalizada = sc.nextLine()
+                meuJogo?.descricao = descricaoPersonalizada
+            } else {
+                meuJogo?.descricao = meuJogo?.titulo
+            }
+
+            gamer.jogosBuscados.add(meuJogo)
         }
+
+        println("Deseja buscar um novo jogo? (S/N)")
+        val resposta = sc.nextLine()
+
+        } while (resposta.equals("S", true))
+
+    println("Jogos buscados:")
+    println(gamer.jogosBuscados)
+
+    println("\nJogos ordenados por título: ")
+    gamer.jogosBuscados.sortBy {
+        it?.titulo
     }
+
+    gamer.jogosBuscados.forEach {
+        println("Titulo: " + it?.titulo)
+    }
+
+    val jogosFiltrados = gamer.jogosBuscados.filter {
+        it?.titulo?.contains("batman", true) ?: false
+    }
+
+    println("\nJogos Filtrados:")
+    println(jogosFiltrados)
+
+    println("Deseja excluir algum jogo da lista original? (S/N)")
+    val opcao = sc.nextLine()
+    if(opcao.equals("s", true)) {
+        println(gamer.jogosBuscados)
+        println("\nInforme a posição do jogo que deseja excluir: ")
+        val posicao = sc.nextInt();
+        gamer.jogosBuscados.removeAt(posicao)
+    }
+
+    println("\nLista atualizada:")
+    println(gamer.jogosBuscados)
+
+    println("Busca finalizada com sucesso!")
 }
--- src/main/kotlin/br/com/alura/alugames/model/Jogo.kt
+++ src/main/kotlin/br/com/alura/alugames/model/Jogo.kt
@@ -5,10 +5,10 @@ data class Jogo(val titulo:String,
     var descricao:String? = null
 
     override fun toString(): String {
-        return "Meu br.com.alura.alugames.model.Jogo:\n" +
+        return "Jogo:\n" +
                 "Titulo: $titulo \n" +
                 "Capa: $capa \n" +
-                "Descricao: $descricao \n"
+                "Descricao: $descricao"
     }
 
 
