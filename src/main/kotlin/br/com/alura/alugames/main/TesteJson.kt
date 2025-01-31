38a7a44a8d5b6836ed2167141680da404c144b84
Lucas Silva <lucas.cristiano96@gmail.com>
2025-01-30 22:05:34 -0300

    Implementado aluguel de jogos

--- src/main/kotlin/br/com/alura/alugames/main/TesteJson.kt
+++ src/main/kotlin/br/com/alura/alugames/main/TesteJson.kt
@@ -1,13 +1,36 @@
 package br.com.alura.alugames.main
 
+import br.com.alura.alugames.model.Periodo
 import br.com.alura.alugames.service.ConsumoApi
+import java.time.LocalDate
 
 fun main() {
     val consumo = ConsumoApi()
     val listaGamers = consumo.buscaGamers()
-    val jogoApi = consumo.buscaJogo("100")
     val listaJogos = consumo.buscaJogosJson()
-    println(listaGamers)
-    println(jogoApi)
-    println(listaJogos)
+
+//    println(listaGamers)
+//    println(listaJogos)
+
+    val gamerCaroline = listaGamers.get(3)
+    val jogoResidentVillage = listaJogos.get(10)
+    val jogoSpider = listaJogos.get(13)
+    val jogoTheLast = listaJogos.get(2)
+
+//    println(gamerCaroline)
+//    println(jogoResidentVillage)
+
+    val periodo1 = Periodo(LocalDate.now(), LocalDate.now().plusDays(1))
+    val periodo2 = Periodo(LocalDate.now(), LocalDate.now().plusDays(10))
+    val periodo3 = Periodo(LocalDate.now(), LocalDate.now().plusDays(7))
+    val periodo4 = Periodo(LocalDate.of(2024,8,1), LocalDate.of(2024,8,15))
+
+    gamerCaroline.alugaJogo(jogoResidentVillage, periodo1)
+    gamerCaroline.alugaJogo(jogoSpider, periodo2)
+    gamerCaroline.alugaJogo(jogoTheLast, periodo3)
+    gamerCaroline.alugaJogo(jogoSpider, periodo4)
+
+    println(gamerCaroline.jogosAlugados)
+
+    println(gamerCaroline.jogosDoMes(1))
 }
--- src/main/kotlin/br/com/alura/alugames/model/Gamer.kt
+++ src/main/kotlin/br/com/alura/alugames/model/Gamer.kt
@@ -15,8 +15,8 @@ data class Gamer(var nome: String, var email: String) {
         }
     var idInterno: String? = null
         private set
-
     val jogosBuscados = mutableListOf<Jogo?>()
+    val jogosAlugados = mutableListOf<Aluguel>()
 
     constructor(nome: String, email: String, dataNascimento:String, usuario:String): this(nome, email) {
         this.dataNascimento = dataNascimento
@@ -49,6 +49,19 @@ data class Gamer(var nome: String, var email: String) {
         } else {
             throw IllegalArgumentException("Email inválido")
         }
+    }
+
+    fun alugaJogo(jogo: Jogo, periodo: Periodo): Aluguel {
+        val aluguel = Aluguel(this, jogo, periodo)
+        jogosAlugados.add(aluguel)
+
+        return aluguel
+    }
+
+    fun jogosDoMes(mes: Int): List<Jogo> {
+        return jogosAlugados
+            .filter { aluguel -> aluguel.periodo.dataInicial.monthValue == mes }
+            .map { aluguel -> aluguel.jogo }
     }
 
     companion object {
